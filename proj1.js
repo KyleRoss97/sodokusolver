7fed8d4d3ce13251c039c5cb4ddfbd259b9cd73d
Kyle <sdjlaksdlsjk@noreply.com>
2022-05-10 21:31:39 -0400

    update2

--- proj1.js
+++ proj1.js
@@ -1,7 +1,8 @@
 let cell={
-    value:0,
+    value:1,
     checked:false
 }
+
 let grid =[[cell,cell,cell,cell,cell,cell,cell,cell,cell],
            [cell,cell,cell,cell,cell,cell,cell,cell,cell],
            [cell,cell,cell,cell,cell,cell,cell,cell,cell],
@@ -13,7 +14,33 @@ let grid =[[cell,cell,cell,cell,cell,cell,cell,cell,cell],
            [cell,cell,cell,cell,cell,cell,cell,cell,cell]];
 
 let iter=0;
+let guessvalue=1;
+for (let i=0;i<9;i++){
+    for(let j=0;j<9;j++){
+        grid[i][j].value= 0;
+    }
+}
+console.log(grid);
+//loops through all cells
+
+for (let i=0;i<9;i++){
+    for(let j=0;j<9;j++){
+        //vertical check
+        for(let k=0;k<9;k++){
+            if(grid[i][k].value==guessvalue){
+                guessvalue++;
+                k=0;
+            }
+        }
+
+        //horizontal
+        for(let k=0;k<9;k++){
+            if(grid[i][k].value==guessvalue){
+                guessvalue++;
+                k=0;
+            }
+        }
 
 
-
-console.log(grid[0][0]);
+    }
+}
